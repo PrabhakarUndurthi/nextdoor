55d4ed5f2a941da40a84d86622eefe2a3f7a4a68
Prabhakar Undurthi <undurthi.prabhakar@gmail.com>
2016-03-08 13:00:54 +0530

    added tests for users first_name, last_name, and prfile_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,14 @@ class User < ActiveRecord::Base
 
     has_many :statuses
 
-
+    validates :first_name, :last_name, :user_name, presence:true
+    
+    validates :user_name, presence:true, uniqueness:true
+    
+    validates :user_name, format: {
+    with: /\A[a-zA-Z\-\_]+\Z/,
+    message: "must be formatted correctly."
+  }
     def full_name
     	first_name + " " + last_name
     end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+  test "a user should enter last_name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+  test "a user should enter user_name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:user_name].empty?
+  end
+  test "a user should enter a unique user_name" do
+  	user = User.new
+  	user.user_name = users(:tom).user_name
+  	assert !user.save
+  	assert !user.errors[:user_name].empty?
+  end
+  test "user must enter a user_name without any spaces" do
+  	user = User.new
+  	user.user_name = "My username with spaces"
+  	assert !user.save
+  	assert !user.errors[:user_name].empty?
+  	assert user.errors[:user_name].include?("must be formatted correctly.")	
+  end
 end
